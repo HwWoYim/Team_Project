432b407d917bd9a22029dda5a3b0719fca187d23
HwWoYim <yimhw77@naver.com>
2022-09-25 19:10:30 +0900

    4번 째 팀 프로젝트 / JSP / VSC,Eclipse / slack / Oracle / jQuery / 필터검색 / 반응형 웹

---
+++ 4차 JSP MVC2 패턴 팀 프로젝트/src/main/java/action/InsertMemberAction.java
@@ -0,0 +1,38 @@
+package action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import member.MemberDAO;
+import member.MemberVO;
+
+public class InsertMemberAction implements Action{
+
+   @Override
+   public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+      ActionForward forward = null;
+      
+      MemberDAO mdao = new MemberDAO();
+      MemberVO mvo = new MemberVO();
+      
+      mvo.setMid(request.getParameter("mid"));
+      mvo.setMpw(request.getParameter("mpw"));
+      mvo.setMname(request.getParameter("mname"));
+      mvo.setMnickname(request.getParameter("mnickname"));
+      mvo.setMadd(request.getParameter("madd"));
+      mvo.setMemail(request.getParameter("memail"));
+      mvo.setMphone(request.getParameter("mphone"));
+      mvo.setMrole("일반회원");
+      
+      if(mdao.insert(mvo)) {
+      forward = new ActionForward();
+      forward.setPath("main.do");
+      forward.setRedirect(true);
+      } else {
+         throw new Exception("insertMemberAction 오류");
+      }
+      // TODO Auto-generated method stub
+      return forward;
+   }
+   
+}
